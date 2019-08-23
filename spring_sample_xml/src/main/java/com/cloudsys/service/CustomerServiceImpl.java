1251b0c3c6836d9e5d6be8c78957b34520256d65
Ashok <ashok.alexadev@gmail.com>
2019-08-22 23:59:57 -0700

    Constructor Injection Example

--- spring_sample_xml/src/main/java/com/cloudsys/service/CustomerServiceImpl.java
+++ spring_sample_xml/src/main/java/com/cloudsys/service/CustomerServiceImpl.java
@@ -9,11 +9,31 @@ public class CustomerServiceImpl implements CustomerService {
 	
 	private CustomerRepository customerRepository; 
 	
+	/**
+	 * Default Constructor
+	 */
+	public CustomerServiceImpl() {
+		
+	}
+	
+	/**
+	 * Constructor Injection to inject customerRepository
+	 * @param customerRepository
+	 */
+	public CustomerServiceImpl(CustomerRepository customerRepository) {
+		this.customerRepository=customerRepository;
+	}
+	
+	
 	@Override
 	public List<Customer> findAll(){
 		return customerRepository.findAll();
 	}
-
+	
+	/**
+	 * Setter Injection to inject customerRepository
+	 * @param customerRepository
+	 */
 	public void setCustomerRepository(CustomerRepository customerRepository) {
 		this.customerRepository = customerRepository;
 	}
